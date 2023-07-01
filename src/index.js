48a5c2ca8f923124f7a688fd1ed3a68889a1ffe4
Ruma <matteoh8@hotmail.it>
2023-07-01 22:08:21 +0200

    aggiunti terminali

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+//$ Render utilizzando la Concurrent Mode
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+//$ Legacy mode
+// ReactDOM.render(<App />, document.getElementById('root'));
