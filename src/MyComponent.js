aa0b7dc414173acf33951b248ecce3036baca638
Ruma <matteoh8@hotmail.it>
2023-07-02 00:08:45 +0200

    Sintassi JSX completata

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,8 +1,54 @@
 import React from "react";
 const MyComponent = () => {
    return <div>Hello World</div>;
-}
+};
 export default MyComponent;
 
 
+{/* <div>Hello World</div> */}
+{/* <br></br> */}
+{/* <br /> */}
+{/* Questo è un commento */}
+{/* <div className="container">Contenuto</div>; */}
+{/* const name = "Mario";
+const element = <h1>Ciao, {name}!</h1>;
+return element; */}
+{/* const style = {
+   color: 'red',
+   fontSize: '24px'
+};
+const element = <p style={style}>Testo in rosso</p>;
+return element; */}
+{/* const element = (
+   <label htmlFor="example">
+     <input type="checkbox" defaultChecked /> Esempio
+   </label>
+);
+return element; */}
 
+{/*const items = ['apple', 'banana', 'cherry'];
+return (
+   <ul>
+      {items.map((item, index) => (
+         <li key={index}>{item}</li>
+      ))}
+   </ul>
+); */}
+
+
+/* const handleClick = () => {
+   console.log('Button clicked');
+};
+   return (
+   <button onClick={handleClick}>Click me</button>
+); */
+
+{/* <div>
+   <h1>Hello World</h1>
+</div> */}
+
+{/* return (
+   <React.Fragment>
+      <h1>Hello World</h1>
+   </React.Fragment>
+); */}
